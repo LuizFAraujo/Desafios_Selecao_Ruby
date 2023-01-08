5af99eddcc53f01b65cbea96b1a36daffbed917d
Luiz Fernando de Araujo <luizfaraujo.eng@gmail.com>
2023-01-08 02:17:15 -0300

    Exercício 02.

---
+++ 02-calculator/projeto/lib/calculadora.rb
@@ -0,0 +1,16 @@
+def soma(numA, numB)
+    numA + numB
+end
+
+def subtracao(numA, numB)
+    numA - numB
+end
+
+def multiplicacao(numA, numB)
+    numA * numB
+end
+
+def divisao(numA, numB)
+    return 'Opa! Zero como divisor' if numB == 0
+    numA / numB
+end
